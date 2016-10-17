576dbc6e80f9364257e599507c482df2e8c2d6c2
naveenarun <notatroll.troll@gmail.com>
2016-10-17 01:36:43 -0700

    lol

--- conserWater/google_maps_prototype/js/test.js
+++
@@ -1,2 +0,0 @@
-currMarker=badMarkers.pop();currMarker.setOpacity(0.5);goodMarkers.push(currMarker);myInfoBoxes.pop().close();currMarker.addListener("click",
-function(event){popupCloseBox(this);});
